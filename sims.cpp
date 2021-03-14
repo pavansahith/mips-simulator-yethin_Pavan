d3ce35f10af0a6d6558560c83078078440fddcb6
pavansahith <cs19b017@iittp.ac.in>
2021-03-14 22:16:56 +0530

    init

--- sims.cpp
+++ sims.cpp
@@ -17,3 +17,30 @@ struct memory_elements{
     string name;
     int start_address;
 };
+
+
+class simulator{
+    public:
+    string registers[32]={"zero","at","v0","v1","a0","a1","a2","a3","t0","t1","t2","t3","t4","t5","t6",
+	"t7","s0","s1","s2","s3","s4","s5","s6","s7","t8","t9","k0","k1","gp","sp","s8","ra"};
+    int value_of_registers[32];
+    vector<string> program;
+    int number_of_instructions;
+    string present_instruction;
+    int memory_element_values[1000];
+    void read_instruction(int line_number);
+    int registers_in_present_instruction[3];
+    string operations_allowed[17]={"add","sub","mul","and","or","nor","slt","addi","andi","ori","slti","lw",
+	"sw","beq","bne","j","la"};
+    int pc ;
+    int position_of_element_in_memory;
+    vector<struct labels>labels_in_program;
+    vector<struct memory_elements>memory_used_in_program;
+    void RemoveSpaces(string &str);
+    void display();
+    void run();
+    void execute_present_operation(int n);  //******
+    void valid_register(string s,int n);
+    simulator(ifstream& file);
+
+};
